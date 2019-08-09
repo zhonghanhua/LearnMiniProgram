bf0eccd0556077ac99edbf253c056c094e5d03e0
zhonghanhua <939359076@qq.com>
2019-08-09 17:55:13 +0800

    小程序的初体验

--- pages/about/about.js
+++ pages/about/about.js
@@ -5,62 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:'zhonghanhua',
+    age:22,
+    students:[
+      { id: 111, name: 'aaa', age: 10 },
+      { id: 222, name: 'bbb', age: 20 },
+      { id: 333, name: 'ccc', age: 30 }
+    ],
+    counters:0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // ＋按钮监听
+  handleBtnClick(){
+    // 1.错误做法，不会刷新界面
+    // this.data.counters += 1
 
+    // this.setData()
+    this.setData({
+        counters : this.data.counters + 1 
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // - 按钮监听
+  handleSubtraction(){
+    this.setData({
+      counters: this.data.counters - 1
+    })
   }
+
 })
